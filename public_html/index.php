05336621a874730978096c1fa94a6844eaf17e9a
kaushal <swastik1706@gmail.com>
2017-10-29 19:44:14 +0000

    First Commit

---
+++ public_html/index.php
@@ -0,0 +1,13 @@
+<?php
+    // load up your config file
+    require_once($_SERVER["DOCUMENT_ROOT"]."/adrs/resources/config.php");
+    require_once(TEMPLATES_PATH . "/header.php");
+?>
+<div id="container">
+    <div id="content">
+        <!-- content -->
+    </div>
+</div>
+<?php
+    require_once(TEMPLATES_PATH . "/footer.php");
+?>
