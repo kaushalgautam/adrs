1ca144d8a43a5184cd744226d5daa0004f1fa5bc
kaushal <swastik1706@gmail.com>
2017-11-09 06:35:09 +0000

    Made the homepage... kinda made the homepage

--- public_html/index.php
+++ public_html/index.php
@@ -3,9 +3,10 @@
     require_once($_SERVER["DOCUMENT_ROOT"]."/adrs/resources/config.php");
     require_once(TEMPLATES_PATH . "/header.php");
 ?>
-<div id="container">
-    <div id="content">
+<div class="jumbotron jumbotron-fluid">
+    <div class="container-fluid">
         <!-- content -->
+        <h1 class = "display-3">Hello World</h1>
     </div>
 </div>
 <?php
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -8,19 +8,14 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 </head>
 
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
-  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="#">Navbar</a>
-</nav>
+<!--<nav class="navbar navbar-toggleable-md navbar-light bg-faded">-->
+<!--  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--    <span class="navbar-toggler-icon"></span>-->
+<!--  </button>-->
+<!--  <a class="navbar-brand" href="#">Navbar</a>-->
+<!--</nav>-->
 
 <body>
-<div id="header">
-    <h1>Simple Site</h1>
-    <ul class="nav global">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Articles</a></li>
-        <li><a href="#">Portfolio</a></li>
-    </ul>
+<div id="header" class = "container-fluid" align = "center">
+    <h1 class = "display-3">Alumni Document Retrieval System</h1>
 </div>
